a0ab015ffdca166dd5686b93c84de07d3e62ec38
TrainzC99 <covalciuc_victor@protonmail.com>
2025-12-18 18:54:30 +0200

    Added modifcation for part 2
    - The modification simply consists in modifying the regex expression
    to match on "do()" or "don't()" or on any "mul()" instruction.
    - Also made appropriate modifications to only do math on the "mul()"
    instruction when a "do()" indicates so.

--- sol-2/d3aoc.c
+++ sol-2/d3aoc.c
@@ -7,131 +7,143 @@
 
 #define FSEEK_OFFSET 0
 #define MAX_MATCH 5
+#define DO_CHAR_COUNT 4
+#define DONT_CHAR_COUNT 7
 
 int main (int argc, const char* argv[])
 {
-	/* File variables */
-	FILE *inFile;
-	unsigned long long fileSize;
-	char *fileText, *fileTextBegin;
-	int pLength;
+    /* File variables */
+    FILE *inFile;
+    unsigned long long fileSize;
+    char *fileText, *fileTextBegin;
+    int pLength;
 
-	/* Match and related variables */
-	regex_t matchex;
-	regmatch_t matches[MAX_MATCH];
-	char *mStr, *pChrNum;
-	const char *nums = "0123456789";
-	int mCount;
+    /* Match and related variables */
+    regex_t matchex;
+    regmatch_t matches[MAX_MATCH];
+    char *mStr, *pChrNum;
+    const char *nums = "0123456789";
+    int mCount;
+    bool math;
 
-	/*Math operations variables*/
-	int no1, no2;
-	unsigned long long sum;
+    /*Math operations variables*/
+    int no1, no2;
+    unsigned long long sum;
 
-	// Check for enough args were provided
-	if (argc < 2)
-	{
-		(void) printf("Please provide input file!\n");
-		exit(EXIT_FAILURE);
-	}
+    // Check for enough args were provided
+    if (argc < 2)
+    {
+        (void) printf("Please provide input file!\n");
+        exit(EXIT_FAILURE);
+    }
 
-	// File setup
-	inFile = fopen(argv[1], "rb");
-	checkFileOpen(inFile);
-	fileSize = getFileSize(inFile);
-	// This returns +1 as ftell returns the current value of the pos. indicator
-	// which inturn itself points to the next character to be read or 
-	// written in the next I/O operation.
+    // File setup
+    inFile = fopen(argv[1], "rb");
+    checkFileOpen(inFile);
+    fileSize = getFileSize(inFile);
+    // This returns +1 as ftell returns the current value of the pos. indicator
+    // which inturn itself points to the next character to be read or
+    // written in the next I/O operation.
 
-	// File details
-	(void) printf("File name -> %s\n", argv[1]);
-	(void) printf("File size -> %llu\n", fileSize);
+    // File details
+    (void) printf("File name -> %s\n", argv[1]);
+    (void) printf("File size -> %llu\n", fileSize);
 
-	// Read and store file in memory
-	fileText = malloc(sizeof(*fileText) * fileSize);
-	if(fileText == NULL)
-	{
-		(void) fprintf(stderr, "Error while allocating memory!\n");
-		fclose(inFile);
-		exit(EXIT_FAILURE);
-	}
-	fileTextBegin = fileText;
-	if(fread(fileText, sizeof(*fileText), fileSize, inFile) != fileSize)
-	{
-		(void) fprintf(stderr, "Error file not read correctly!\n");
-		fclose(inFile);
-		free(fileText);
-		exit(EXIT_FAILURE);
-	}
-	fileText[fileSize] = '\0';
-	
-	// Display read contents
-	// (void) printf("Read contents:\n");
-	// (void) printf("%s\n", fileText);
+    // Read and store file in memory
+    fileText = malloc(sizeof(*fileText) * fileSize);
+    if(fileText == NULL)
+    {
+        (void) fprintf(stderr, "Error while allocating memory!\n");
+        fclose(inFile);
+        exit(EXIT_FAILURE);
+    }
+    fileTextBegin = fileText;
+    if(fread(fileText, sizeof(*fileText), fileSize, inFile) != fileSize)
+    {
+        (void) fprintf(stderr, "Error file not read correctly!\n");
+        fclose(inFile);
+        free(fileText);
+        exit(EXIT_FAILURE);
+    }
+    fileText[fileSize] = '\0';
 
-	// Process using RegEx
-	if(regcomp(&matchex, \
-				"mul([0-9]\\{1,3\\},[0-9]\\{1,3\\})", \
-				REG_ICASE | REG_NEWLINE) != 0)
-	{
-		(void) printf("Error when processing regex\n");
-		exit(EXIT_FAILURE);
-	}
+    // Display read contents
+    // (void) printf("Read contents:\n");
+    // (void) printf("%s\n", fileText);
 
-	sum = 0;
-	mCount = 0;
-	// Match regex patter on input text
-	while((regexec(&matchex, fileText, MAX_MATCH, matches, 0) == 0) \
-			&& (*fileText != '\0'))
-	{
-		(void) printf("Match found: %.*s\n", 
-				matches[0].rm_eo - matches[0].rm_so,
-				fileText + matches[0].rm_so);
-		mCount++;
+    // Process using RegEx
+    if(regcomp(&matchex, \
+               "\\(do()\\|don't()\\)\\|\\(mul([0-9]\\{1,3\\},[0-9]\\{1,3\\})\\)", \
+               REG_ICASE | REG_NEWLINE) != 0)
+    {
+        (void) printf("Error when processing regex\n");
+        exit(EXIT_FAILURE);
+    }
 
-		// Get string of match
-		mStr = fileText + matches[0].rm_so;
+    sum = 0;
+    mCount = 0;
+    math = TRUE;
+    // Match regex patter on input text
+    while((regexec(&matchex, fileText, MAX_MATCH, matches, 0) == 0) \
+            && (*fileText != '\0'))
+    {
+        (void) printf("Match found: %.*s\n",
+                      matches[0].rm_eo - matches[0].rm_so,
+                      fileText + matches[0].rm_so);
+        mCount++;
 
-		// Parse string
-			// Procedure:
-			// Look for numbers
-			// pChrNum & pLength get the whole number
-			// Extract digits and turn them into numbers
-			
-			// First number
-			pChrNum = strpbrk(mStr, nums);
-			pLength = strspn(pChrNum, nums);
-			no1 = atoi(pChrNum); 
-			// this stops at the first non-numerical char.
-			// see glibc (strtol_l) impl. for details
+        // Get string of match
+        mStr = fileText + matches[0].rm_so;
+        if(strncmp("do()", mStr, DO_CHAR_COUNT) == 0)
+            math = TRUE;
+        else if(strncmp("don't()", mStr, DONT_CHAR_COUNT) == 0)
+            math = FALSE;
+        else if(math == TRUE)
+        {
+            // Parse string
+            // Procedure:
+            // Look for numbers
+            // pChrNum & pLength get the whole number
+            // Extract digits and turn them into numbers
 
-			// Second number
-			pChrNum = pChrNum + pLength;
-			pChrNum = strpbrk(pChrNum, nums);
-			no2 = atoi(pChrNum);
-			
-		// Multiply and Add numbers together
-		sum += (no1 * no2);
+            // First number
+            pChrNum = strpbrk(mStr, nums);
+            pLength = strspn(pChrNum, nums);
+            no1 = atoi(pChrNum);
+            // this stops at the first non-numerical char.
+            // see glibc (strtol_l) impl. for details
 
-		// Move past the match
-		fileText = fileText + matches[0].rm_eo;
+            // Second number
+            pChrNum = pChrNum + pLength;
+            pChrNum = strpbrk(pChrNum, nums);
+            no2 = atoi(pChrNum);
 
-		// In case of 0-length matches, avoiding infinite loop
-		if(matches[0].rm_eo == matches[0].rm_so)
-			fileText++;
-	}
+            // Multiply and Add numbers together
+            sum += (no1 * no2);
+        }
+        // Move past the match
+        fileText = fileText + matches[0].rm_eo;
 
-	printf("Matches found -> %d\n", mCount);
-	printf("Sum of all mul instructions is -> %llu\n", sum);
+        // In case of 0-length matches, avoiding infinite loop
+        if(matches[0].rm_eo == matches[0].rm_so)
+            fileText++;
 
-	// Free file and regex from memory
-	free(fileTextBegin);
-	fclose(inFile);
-	regfree(&matchex);
+    }
 
-	return EXIT_SUCCESS;
+    printf("Matches found -> %d\n", mCount);
+    printf("Sum of all mul instructions is -> %llu\n", sum);
+
+    // Free file and regex from memory
+    free(fileTextBegin);
+    fclose(inFile);
+    regfree(&matchex);
+
+    return EXIT_SUCCESS;
 }
 
-inline int mul(int X, int Y) { return X * Y; }
+inline int mul(int X, int Y) {
+    return X * Y;
+}
 
 /**
  * @brief This function obtains the size of a given file
@@ -141,16 +153,16 @@ inline int mul(int X, int Y) { return X * Y; }
  */
 unsigned long long getFileSize(FILE* fp)
 {
-	unsigned long long size = 0;
-	 //This moves the FP (file pointer) to the EOF (end of file)
-	//which can be made use of later in the program
-	fseek(fp, FSEEK_OFFSET, SEEK_END);
-	//This return the pointer's position in the file
-	size = ftell(fp);
-	//This moves back the FP to the start of the file (SOF)
-	rewind(fp);
+    unsigned long long size = 0;
+    //This moves the FP (file pointer) to the EOF (end of file)
+    //which can be made use of later in the program
+    fseek(fp, FSEEK_OFFSET, SEEK_END);
+    //This return the pointer's position in the file
+    size = ftell(fp);
+    //This moves back the FP to the start of the file (SOF)
+    rewind(fp);
 
-	return size;
+    return size;
 }
 
 /**
@@ -161,10 +173,10 @@ unsigned long long getFileSize(FILE* fp)
  */
 void checkFileOpen(FILE *ifp)
 {
-	if(ifp == NULL)
-	{
-		fprintf(stderr, "The file is unopenable!\n");
-		exit(EXIT_FAILURE);
-	}
+    if(ifp == NULL)
+    {
+        fprintf(stderr, "The file is unopenable!\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
